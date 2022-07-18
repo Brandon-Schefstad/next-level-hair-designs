5722b6d4a9b728bb19ad22f9622d245fd4b781a1
Brandon-Schefstad <bschefstad@gmail.com>
2022-07-18 13:04:45 -0400

    Feat: Added Policies to Booking Page

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Iframe from './Iframe';
 import Header from './Header';
+import Policies from './Policies';
 
 const Booking = () => {
 	return (
 		<div className="wrapper">
 			<Header button="Return Home" route="/" />
+			<Policies />
 			<Iframe />
 		</div>
 	);
